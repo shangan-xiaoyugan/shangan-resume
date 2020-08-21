121ba66f98aa3273c72bf476b8eb94a7ef83404b
shangan-xiaoyugan <62691539+shangan-xiaoyugan@users.noreply.github.com>
2020-08-21 02:41:24 +0000

    Deploying to gh-pages from  @ b687314db20196ba231ad328a9c91e29f23a722b 🚀

---
+++ precache-manifest.268ab94cdd613d2940ec91ada52ba284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2518385e35306083bff4734a479fc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d6f24120c6793a9737",
+    "url": "/static/css/main.b52b6472.chunk.css"
+  },
+  {
+    "revision": "a963715952b94adb4699",
+    "url": "/static/js/2.05116abc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.05116abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d6f24120c6793a9737",
+    "url": "/static/js/main.a3d71323.chunk.js"
+  },
+  {
+    "revision": "43a62b9c014d0ba94a80",
+    "url": "/static/js/runtime-main.60a07d1e.js"
+  }
+]);
